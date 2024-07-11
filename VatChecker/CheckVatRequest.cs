8d626069de56de56686a882fe2d3218e0a452e72
Jim Wang <Jim.wang@spray.com>
2024-07-11 15:05:38 -0500

    first

---
+++ VatChecker/CheckVatRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VatChecker;
+
+public class CheckVatRequest
+{
+    public string countryCode { get; set; }
+    public string vatNumber { get; set; }
+    public string requesterMemberStateCode { get; set; }
+    public string requesterNumber { get; set; }
+    public string traderName { get; set; }
+    public string traderStreet { get; set; }
+    public string traderPostalCode { get; set; }
+    public string traderCity { get; set; }
+    public string traderCompanyType { get; set; }
+}
